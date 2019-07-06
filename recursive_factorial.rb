a3a7554bbf333c6e768471a623e5f3633e296895
Learningstuff98 <42154066+Learningstuff98@users.noreply.github.com>
2019-07-05 19:32:28 -0700

    Update recursive_factorial.rb

--- recursive_factorial.rb
+++ recursive_factorial.rb
@@ -6,20 +6,3 @@ def factorial(num, acc = 1)
     factorial(num - 1, acc)
   end
 end
-
-def assert_equal(actual, expected)
-  if actual == expected
-    puts "Passed"
-  else
-    puts "Failed. Expected #{expected}, but got #{actual}."
-  end
-end
-  
-assert_equal(factorial(1), 1)
-assert_equal(factorial(2), 2)
-assert_equal(factorial(3), 6)
-assert_equal(factorial(4), 24)
-assert_equal(factorial(5), 120)
-assert_equal(factorial(6), 720)
-assert_equal(factorial(7), 5040)
-assert_equal(factorial(8), 40320)
